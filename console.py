8e84a9c7024b51e4d69ca79dde7c5dcc4fac42e0
egbuniwecemeka <egbuniwecemeka.com>
2024-09-18 15:27:47 +0100

    create method

--- console.py
+++ console.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
 """ The entry point to a command interpreter """
 
-
 import cmd
+from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """ HBNB Command Interpreter class """
     prompt = '(hbnb) '
+
+    def do_create(self, line):
+        """ Creates a new instance, saves to (JSON file) and print it's id """
+        new_instance = BaseModel()
+        new_instance.save()
+        print(new_instance.id)
+
 
     def do_help(self, line):
         """ List all interpreter commands"""
