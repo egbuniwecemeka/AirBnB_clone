a7cea02964050a2922fb9e8d2877cc8482b786ce
egbuniwecemeka <egbuniwecemeka.com>
2024-09-28 16:52:35 +0100

    base_model file added

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,56 @@
-#!/usr/bin/python3
-"""Defines the BaseModel class."""
-
-from uuid import uuid4
-from datetime import datetime
+import uuid
+import datetime
 from models import storage
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
-
+    """A base class for all common attributes/methods for other classes"""
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+            """Initializes public instance attributes
 
-        Args:
-            *args (any): Unused.
-            **kwargs (dict): Key/value pairs of attributes.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = self.created_at
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self.__dict__[k] = datetime.strptime(v, tform)
-                else:
-                    self.__dict__[k] = v
-        else:
-            storage.new(self)
+            Args:
+                 *args: Variable length list
+                 *kwargs: keyword arguments for deserialization
+            
+            """
+            if kwargs:
+                 # To handle deserializtion, assign values from dictionary
+                 for key,value in kwargs.items():
+                      if key != '__class__':
+                           setattr(self, key, value)
+               # converts string dates back to datetime objects
+                 self.created_at = datetime.datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+                 self.updated_at = datetime.datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
+            else:
+               # Normal initialization
+               self.id = str(uuid.uuid4())
+               self.created_at = datetime.datetime.now()
+               self.updated_at = self.created_at
+
+               # Add this new instance to storage
+               storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
-        self.updated_at = datetime.now()
-        storage.save()
+         """Updates current datetime anytime an object is changed"""
+         self.updated_at = datetime.datetime.now()
+         storage.save()
 
-    def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-
-        Includes the key/value pair __class__ representing
-        the class name of the object.
-        """
-        rdict = self.__dict__.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["__class__"] = self.__class__.__name__
-        return rdict
-
+    
     def __str__(self):
          """Returns a string representation of BaseModel instance"""
-         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+         return (f"[{self.__class__.__name__}] ({self.id}) "
+                 f"{{'my_number': {self.my_number}, 'name': {self.name}, "
+                 f"'updated_at': {repr(self.updated_at)}, 'id': '{self.id}', 'created_at': {repr(self.created_at)}}}"
+          )
+    
+    def to_dict(self):
+         """Returns a dictionary containing all key, value pairs of __dict__ of the object's instance
+
+         class name is added to dictionary and datetime object is converted to ISO string format for serialization
+         """
+         return {'my_number': getattr(self, 'my_number', 0),
+                 'name': getattr(self, 'name', ''),
+                 '__class__': self.__class__.__name__,
+                 'updated_at': self.updated_at.isoformat(),
+                 'id': self.id,
+                 'created_at': self.created_at.isoformat()
+                 }
